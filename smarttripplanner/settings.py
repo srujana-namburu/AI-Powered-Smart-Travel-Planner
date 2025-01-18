523565b17af96128544e1d605a4e768e2f99c9b2
srujana-namburu <117283542+srujana-namburu@users.noreply.github.com>
2025-01-18 09:52:38 +0530

    Update settings.py

--- smarttripplanner/settings.py
+++ smarttripplanner/settings.py
@@ -49,9 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'smarttripplanner.urls'
 STATICFILES_DIRS = [BASE_DIR/ 'frontend/static']
