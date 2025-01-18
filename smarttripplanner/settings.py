233b7d9afa9c441e9a3903792c79909224579084
srujana-namburu <117283542+srujana-namburu@users.noreply.github.com>
2025-01-18 11:42:23 +0530

    Update settings.py

--- smarttripplanner/settings.py
+++ smarttripplanner/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = ' /static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = {
     os.path.join(BASE_DIR, 'public/static')
 }
